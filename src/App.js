b64c3ef147080ff32b7d4e2d1586060ca039f21b
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-04-17 21:03:06 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
+import { BrowsweRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import CreateBook from './Components/CreateBook';
+import ShowBookList from './Components/ShowBookList';
+import ShowBookDetails from './Components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element ={<ShowBookList/>} />
+          <Route path='/create-book' element={<CreateBook/>}/>
+          <Route path='/edit-book/:id' element={<UpdateBookInfo/>} />
+          <Router path='/show-book/:id' element={<ShowBookList/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
