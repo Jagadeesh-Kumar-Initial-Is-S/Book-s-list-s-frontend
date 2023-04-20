23a2acf0f34e0eae6279fc81630d6226fd183cbe
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-04-20 10:11:07 +0530

    Commit

--- src/Components/CreateBook.js
+++ src/Components/CreateBook.js
@@ -1,9 +1,62 @@
 import React, { useState } from 'react';
-import { Link } 
+import { Link } from 'reacct-router-dom';
+import axios from 'axios';
 
-function CreateBook() {
+import { useNavigate } from 'react-router-dom';
+
+function CreateBook(props) {
+  // Define the state with useState hook
+  const navigate = useNavigate();
+  const [book, setBook] = userState({
+    title: '',
+    isbn: '',
+    author: '',
+    description: '',
+    published_date: '',
+    publisher: '',
+  });
+
+  const on Change = (e) => {
+    setBook({ ...book, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post('http://localhost:8082/api/books', book)
+      .then((res) => {
+        setBook({
+          title: '',
+          isn: '',
+          author: '',
+          description: '',
+          published_date: '',
+          publisher: '',
+        });
+
+        // Push to
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log('Error in CreateBook!');
+      });
+  };
+
+
   return (
-    <div>CreateBook</div>
+    <div className='CreateBook'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-8 m-auto'>
+            <br />
+            <Link to='/' className='btn btn-outline-warning float-left'>
+              Show Book List
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
