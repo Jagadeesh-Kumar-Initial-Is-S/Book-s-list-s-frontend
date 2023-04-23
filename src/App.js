cb9251af6b1dd6205c746e0228d75b1ce8b8c443
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-04-23 17:14:39 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import { BrowsweRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Advertisement from './Components/Advertisement';
 
 import CreateBook from './Components/CreateBook';
 import ShowBookList from './Components/ShowBookList';
@@ -10,14 +11,16 @@ import UpdateBookInfo from './components/UpdateBookInfo';
 function App() {
   return (
     <Router>
+      <Advertisement />
       <div>
         <Routes>
-          <Route exact path='/' element ={<ShowBookList/>} />
-          <Route path='/create-book' element={<CreateBook/>}/>
-          <Route path='/edit-book/:id' element={<UpdateBookInfo/>} />
-          <Router path='/show-book/:id' element={<ShowBookDetails/>} />
+          <Route exact path='/' element={<ShowBookList />} />
+          <Route path='/create-book' element={<CreateBook />} />
+          <Route path='/edit-book/:id' element={<UpdateBookInfo />} />
+          <Router path='/show-book/:id' element={<ShowBookDetails />} />
         </Routes>
       </div>
+      <Advertisement />
     </Router>
   );
 }
--- src/Components/ShowBookList.js
+++ src/Components/ShowBookList.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import BookCard from './BookCard';
 
 function ShowBookList() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios
+  })
   return (
     <div>ShowBookList</div>
   )
