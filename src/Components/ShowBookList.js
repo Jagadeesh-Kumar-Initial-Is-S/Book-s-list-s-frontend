cd67ad86f3bf0cf5c1c1f0664752d636166b1fd8
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-04-25 17:08:14 +0530

    Commit

--- src/Components/ShowBookList.js
+++ src/Components/ShowBookList.js
@@ -9,9 +9,45 @@ function ShowBookList() {
 
   useEffect(() => {
     axios
-  })
+      .get('https://book-s-list-s-backend.vercel.app/api/books')
+      .then((res) => {
+        setBooks(res.data);
+      })
+      .catch((err) => {
+        console.log('Error from ShowBookList');
+      });
+  }, []);
+
+  const bookList =
+    books.length === 0
+      ? 'there is no book record!'
+      : books.map((book, k) => <BookCard book={book} key={k} />);
+
   return (
-    <div>ShowBookList</div>
+    <div className='ShowBookList'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <br />
+            <h2 className='display-4 text-center'>Books List</h2>
+          </div>
+
+          <div className='col-md-11'>
+            <Link
+              to='/create-book'
+              className='btn btn-outline-warning float-right'
+            >
+              + Add New Book
+            </Link>
+            <br />
+            <br />
+            <hr />
+          </div>
+        </div>
+
+        <div className='list'>{bookList}</div>
+      </div>
+    </div>
   )
 }
 
