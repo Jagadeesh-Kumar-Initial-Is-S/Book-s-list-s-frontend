61e163177b3a8ee01ff586915b4fa47b46220cd4
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-04-27 17:07:01 +0530

    Commit

--- src/Components/ShowBookDetails.js
+++ src/Components/ShowBookDetails.js
@@ -8,7 +8,42 @@ function ShowBookDetails(props) {
 
   const { id } = useParms();
   const navigate = useNavigate();
-  cons
+  
+  useEffect(() => {
+    axios
+    ..get(`http/api/books/${id}`)
+    .then((res) => {
+      setBook(res.data);
+    })
+    .catch((err) => {
+      console.log('Eror from ShowBookDetails');
+    });
+  },[id]);
+
+  const onDeleteClick = (id) => {
+    axios
+    ...delete(`http:///api/books/${id}`)
+    .then((res) => {
+      navigate('/');
+    })
+    .catch((err) => {
+      console.log('Error from ShowBookDetails_deleteClick');
+    });
+  };
+
+  const BookItem = (
+    <div>
+      <table className='table tble-hover table-dark'>
+        <tbody>
+          <tr>
+            <th scope='row'>1</th>
+            <td>Title</td>
+            <td>{book.title}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
   return (
     <div>ShowBookDetails</div>
   )
