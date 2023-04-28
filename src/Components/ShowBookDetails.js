a0f68aeb8dcbd6ccd9970c73e9f850e8295c1d15
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-04-28 17:42:27 +0530

    Commit

--- src/Components/ShowBookDetails.js
+++ src/Components/ShowBookDetails.js
@@ -11,7 +11,7 @@ function ShowBookDetails(props) {
   
   useEffect(() => {
     axios
-    ..get(`http/api/books/${id}`)
+    .get(`https://book-s-list-s-backend.vercel.app/api/books/${id}`)
     .then((res) => {
       setBook(res.data);
     })
@@ -22,7 +22,7 @@ function ShowBookDetails(props) {
 
   const onDeleteClick = (id) => {
     axios
-    ...delete(`http:///api/books/${id}`)
+    .delete(`https://book-s-list-s-backend.vercel.app/api/books/${id}`)
     .then((res) => {
       navigate('/');
     })
@@ -40,13 +40,75 @@ function ShowBookDetails(props) {
             <td>Title</td>
             <td>{book.title}</td>
           </tr>
+          <tr>
+            <th scope='row'>2</th>
+            <td>Author</td>
+            <td>{book.author}</td>
+          </tr>
+          <tr>
+            <th scope='row'>3</th>
+            <td>ISBN</td>
+            <td>{book.isbn}</td>
+          </tr>
+          <tr>
+            <th scope='row'>4</th>
+            <td>Publisher</td>
+            <td>{book.publisher}</td>
+          </tr>
+          <tr>
+            <th scope='row'>5</th>
+            <td>Published Date</td>
+            <td>{book.published_date}</td>
+          </tr>
+          <tr>
+            <th scope='row'>6</th>
+            <td>Description</td>
+            <td>{book.description}</td>
+          </tr>
         </tbody>
       </table>
     </div>
-  )
+  );
   return (
-    <div>ShowBookDetails</div>
-  )
+    <div className='ShowBookDetails'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-10 m-auto'>
+            <br /> <br />
+            <Link to='/' className='btn btn-outline-warning float-left'>
+              Show Book List
+            </Link>
+          </div>
+          <br />
+          <div className='col-md-8 m-auto'>
+            <h1 className='display-4 text-center'>Book's Record</h1>
+            <p className='lead text-center'>View Book's Info</p>
+            <hr /> <br />
+          </div>
+          <div className='col-md-10 m-auto'>{BookItem}</div>
+          <div className='col-md-6 m-auto'>
+            <button
+            type='buttton'
+            className='btn btn-outline-danger btn-lg btn-block'
+            onClick={() => {
+              onDeleteClick(book._id);
+            }}
+            >
+              Delete Book
+            </button>
+          </div>
+          <div className='col-md-6 m-auto'>
+            <Link
+            to={`/edit-book/${book._id}`}
+            className='btn btn-outline-info btn-lg btn-block'
+            >
+              Edit Book
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ShowBookDetails
